e18fd0238767adceb025c45fe958b86f14f86785
Cole Arendt <cole.arendt@outlook.com>
2018-08-20 17:32:28 -0400

    restructure htmlDependency function

--- R/initShinyCookie.R
+++ R/initShinyCookie.R
@@ -6,19 +6,15 @@ initShinyCookie <- function(id, namespace) {
     stop("Must provide an ID and a namespace when initializing the shinyCookie package")
   }
 
-  htmltools::htmlDependency(
-    name = "shinyCookie",
-    version = packageVersion("shinyCookie"),
-    src = "assets",
-    script = c("js.cookie.js","shinyCookie.js")
-  )
 
   tagList(
-    singleton(tags$head(
-      initResourcePaths()
-      #tags$script(src = 'shinyCookie/js.cookie.js'),
-      #tags$script(src = 'shinyCookie/shinyCookie.js')
-    )),
+    htmltools::htmlDependency(
+      name = "shinyCookie",
+      version = packageVersion("shinyCookie"),
+      src = "www",
+      package = "shinyCookie",
+      script = c("js.cookie.js","shinyCookie.js")
+    ),
     HTML(paste0("<script type=\"text/javascript\">shinyCookie.init('",namespace,"')</script>")),
     div(class="shiny-cookie", id=id)
   )
