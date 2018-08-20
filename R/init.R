8f9f0c0a6513f77f4aa85123088ec35f6279978f
Cole Arendt <cole.arendt@outlook.com>
2018-08-20 17:32:15 -0400

    remove initResourcePaths function

--- R/init.R
+++ R/init.R
@@ -1,11 +1,2 @@
 .global <- new.env()
 
-initResourcePaths <- function() {
-  if (is.null(.global$loaded)) {
-    shiny::addResourcePath(
-      prefix = 'shinyCookie',
-      directoryPath = system.file('assets', package='shinyCookie'))
-    .global$loaded <- TRUE
-  }
-  HTML("")
-}
