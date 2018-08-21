31b8bc60938eca8af4cea6af264efa3627426907
Cole Arendt <cole.arendt@outlook.com>
2018-08-21 09:23:03 -0400

    make timeout configurable to the user

--- R/initShinyCookie.R
+++ R/initShinyCookie.R
@@ -1,7 +1,7 @@
 #' Initialize shinyCookie in an applicatoin's UI
 #'
 #' @export
-initShinyCookie <- function(id, namespace) {
+initShinyCookie <- function(id, namespace, timeout = 500) {
   if (missing(id) || missing(namespace)){
     stop("Must provide an ID and a namespace when initializing the shinyCookie package")
   }
@@ -15,7 +15,14 @@ initShinyCookie <- function(id, namespace) {
       package = "shinyCookie",
       script = c("js.cookie.js","shinyCookie.js")
     ),
-    HTML(paste0("<script type=\"text/javascript\">shinyCookie.init('",namespace,"')</script>")),
+    HTML(
+      paste0(
+        "<script type=\"text/javascript\">shinyCookie.init('",
+        namespace,
+        "', ",
+        timeout,
+        ")</script>")
+      ),
     span(class="shiny-cookie", id=id)
   )
 }
