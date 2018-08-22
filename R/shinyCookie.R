f5bf4ce6518311f6781d348ee1dc344bcf9b5aa2
Cole Arendt <cole.arendt@outlook.com>
2018-08-22 15:32:47 -0400

    add imports

---
+++ R/shinyCookie.R
@@ -0,0 +1,11 @@
+#' @import shiny
+#' @importFrom utils packageVersion
+#' @importFrom htmltools tagList
+#' @importFrom htmltools HTML
+#' @importFrom htmltools span
+"_PACKAGE"
+
+utils::globalVariables(
+  c(
+  )
+)
