e13a35f9b1e31b1b3ad26871e0626f8dfe16547a
Cole Arendt <cole.arendt@outlook.com>
2018-08-22 15:16:06 -0400

    export updateCookie function

--- R/updateCookie.R
+++ R/updateCookie.R
@@ -1,6 +1,6 @@
 #' Update Cookie
 #'
-#'
+#' @export
 updateCookie <- function(session, name, value) {
   if (missing(session) || missing(name) || missing(value)) {
     stop("Must provide a session, a name, and a value")
